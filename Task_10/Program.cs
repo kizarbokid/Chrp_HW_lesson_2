21de10c8596fb7c5a0570980ae72bb34e9c798ba
YakovkinVD <dikobrazzzik709@gmail.com>
2022-10-05 20:53:44 +0900

    Task 10 solved

--- Task_10/Program.cs
+++ Task_10/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Write some three symbols and press ENTER");
+string text = Console.ReadLine();
+
+Console.WriteLine("Now you will see the second sybmbol"); 
+Console.WriteLine(text.Substring(1,1));// Substring (1,1)- подстрока (с 1ого символа начиная обрезать 1 символ)
