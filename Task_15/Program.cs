fc4401133ee9cd35c3ad6d06ecf21127d4038ee7
YakovkinVD <dikobrazzzik709@gmail.com>
2022-10-05 21:57:40 +0900

    Task 15 solved

---
+++ Task_15/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Write("Input weekday's number: ");
+int is_weekday = Convert.ToInt32(Console.ReadLine());
+
+switch (is_weekday) // Проще через if, через switch..case решила в целях практики
+{
+    case 1: Console.WriteLine("Weekday"); break;
+    case 2: Console.WriteLine("Weekday"); break;
+    case 3: Console.WriteLine("Weekday"); break;
+    case 4: Console.WriteLine("Weekday"); break;
+    case 5: Console.WriteLine("Weekday"); break;
+    case 6: Console.WriteLine("Holiday"); break;
+    case 7: Console.WriteLine("Holiday"); break;
+    default: Console.WriteLine("I have just 7 days of the week"); break;
+}
