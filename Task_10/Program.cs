04af2fb504ff53a13afd17917cf7d35225d97e75
YakovkinVD <dikobrazzzik709@gmail.com>
2022-10-05 21:34:31 +0900

    Task 13 solved

--- Task_10/Program.cs
+++ Task_10/Program.cs
@@ -1,6 +1,6 @@
-﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Shift + Alt + F - отформатировать код
 Console.WriteLine("Write some three symbols and press ENTER");
 string text = Console.ReadLine();
 
-Console.WriteLine("Now you will see the second sybmbol"); 
-Console.WriteLine(text.Substring(1,1));// Substring (1,1)- подстрока (с 1ого символа начиная обрезать 1 символ)
+Console.WriteLine("Now you will see the second sybmbol");
+Console.WriteLine(text.Substring(1, 1));// Substring (1,1)- подстрока (с 1ого символа начиная обрезать 1 символ)
---
+++ Task_13/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Write some numbers and press ENTER");
+string text = Console.ReadLine();
+
+if (int.TryParse(text, out var number)) //  TryParse проверяет, можно ли выполнить операцию 
+{ // по преобразованию переменной text в переменную number типа integer. если true, то ..
+    if (text.Length <= 2)
+    {
+        Console.Write("There is no third symbol");
+    }
+    else
+    {
+        Console.WriteLine(text.Substring(2, 1));
+    }
+}
+else Console.Write("This is not a number");
